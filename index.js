02596f17063993d13894d76948dd2307fba89999
naren <joshinaren93@gmail.com>
2019-09-23 15:28:29 +0530

    make it better

--- index.js
+++ index.js
@@ -22,11 +22,15 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("recipe-front/dist/recipe-front"));
   app.get("*", (req, res) => {
     res.sendFile(
-      path.resolve(__dirname, "recipe-front", "dist","recipe-front", "index.html")
+      path.resolve(
+        __dirname,
+        "recipe-front",
+        "dist",
+        "recipe-front",
+        "index.html"
+      )
     );
   });
 }
-
-app.listen(process.env.port, () =>
-  console.log(`Server started on port ${process.env.port}`)
-);
+const port = process.env.port || 5000;
+app.listen(port, () => console.log(`Server started on port ${port}`));
